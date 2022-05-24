63ada29e04f256b5871136a91095419fa21bec21
unknown <taha.iu.bd@gmail.com>
2022-05-24 14:10:56 +0600

    review-face-data

--- src/pages/Home/Reviews.js
+++ src/pages/Home/Reviews.js
@@ -2,14 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { A11y, Autoplay, Navigation, Pagination, Scrollbar } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import fetcher from '../../api'
-import Review from './Review'
 
 const Reviews = () => {
     const [data, setData] = useState([])
 
     useEffect(() => {
         ;(async () => {
-            const res = await fetcher.get('/tools')
+            const res = await fetcher.get('/reviews')
             setData(res.data)
         })()
     }, [])
@@ -46,19 +45,16 @@ const Reviews = () => {
                                     <img
                                         alt="testimonial"
                                         className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
-                                        src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                                        src={p?.img}
                                     />
                                     <p className="leading-relaxed">
-                                        The ferrari triumphs at the sebring 12
-                                        hours in gtd is a great choice for a
-                                        hybrid sedan. It boasts sleek styling,
-                                        generous interior spaceandplenty of
+                                        {p?.review}
                                     </p>
                                     <span className="inline-block h-1 w-10 rounded bg-green-500 mt-2 mb-6"></span>
-                                    <p className="text-gray-500">5.0 stars </p>
+                                    <p className="text-gray-500">{p.rating} </p>
 
-                                    <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">
-                                        taha
+                                    <h2 className="text-gray-100 font-medium title-font tracking-wider text-sm">
+                                        {p?.name}
                                     </h2>
                                     <p className="text-gray-500">
                                         Senior Product Designer
